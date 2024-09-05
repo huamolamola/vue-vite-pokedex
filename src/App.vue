c846eeea28521445ee971f71fabfbab96d94b701
huamolamola <huajiti@odds.team>
2024-09-05 20:25:57 +0700

    feature: add pokedex with filtered list

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,32 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <p>Filter Text: {{ filterText }}</p>
+  <h1>Pokedex</h1>
+  <input type="text" v-model="filterText" />
+  <ul>
+    <li v-for="(pokemon, index) in filteredPokemonList" :key="`poke-${index}`">
+      #{{ pokemon.entry_number }} - {{ pokemon.pokemon_species.name }}
+    </li>
+  </ul>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<script setup>
+import { ref, computed, onMounted } from "vue";
+
+const pokemonList = ref([]);
+const filterText = ref("");
+
+const filteredPokemonList = computed(() =>
+  pokemonList.value.filter((pokemon) =>
+    pokemon.pokemon_species.name.includes(filterText.value)
+  )
+);
+
+onMounted(async () => {
+  const pokeData = await fetch("https://pokeapi.co/api/v2/pokedex/2/").then(
+    (response) => response.json()
+  );
+  pokemonList.value = pokeData.pokemon_entries;
+});
+</script>
+
+<style scoped></style>
